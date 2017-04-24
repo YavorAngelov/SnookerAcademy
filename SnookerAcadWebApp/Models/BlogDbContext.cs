4a7af1f430f83fb664b467401d347eaa8481337c
YavorAngelov <chillandkill@gmail.com>
2017-04-24 08:25:00 +0300

    Add Article Entity

--- SnookerAcadWebApp/Models/BlogDbContext.cs
+++ SnookerAcadWebApp/Models/BlogDbContext.cs
@@ -13,6 +13,8 @@ namespace SnookerAcadWebApp.Models
         {
         }
 
+        public virtual IDbSet<Article> Articles { get; set; }
+
         public static BlogDbContext Create()
         {
             return new BlogDbContext();
