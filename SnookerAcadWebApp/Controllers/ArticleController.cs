f10a9633798e1fadaaae115925f93f0b9817cd1f
YavorAngelov <chillandkill@gmail.com>
2017-04-25 00:23:44 +0300

    Add Article/Delete

--- SnookerAcadWebApp/Controllers/ArticleController.cs
+++ SnookerAcadWebApp/Controllers/ArticleController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,7 +14,7 @@ namespace SnookerAcadWebApp.Controllers
         // GET: Article
         public ActionResult Index()
         {
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("List");
         }
 
         // GET: Article/List
@@ -21,13 +22,98 @@ namespace SnookerAcadWebApp.Controllers
         {
             using (var database = new BlogDbContext())
             {
-                // Get articles from database
                 var articles = database.Articles
                     .Include(a => a.Author)
+                    .OrderByDescending(a => a.Id)
+                    .Take(6)
                     .ToList();
+
                 return View(articles);
             }
-          
+        }
+
+        // GET: Article/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Article/Create
+        [HttpPost]
+        public ActionResult Create(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                using (var database = new BlogDbContext())
+                {
+                    var authorId = database.Users
+                        .Where(u => u.UserName == this.User.Identity.Name)
+                        .First()
+                        .Id;
+
+                    article.AuthorId = authorId;
+
+                    database.Articles.Add(article);
+                    database.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+            }
+
+            return View(article);
+        }
+
+        // GET: Article/Delete
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                var article = database.Articles
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .First();
+
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                return View(article);
+            }
+        }
+
+        // POST: Article/Delete
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            using (var database = new BlogDbContext())
+            {
+                var article = database.Articles
+                    .Where(a => a.Id == id)
+                    .Include(a => a.Author)
+                    .First();
+
+                if (article == null)
+                {
+                    return HttpNotFound();
+                }
+
+                database.Articles.Remove(article);
+                database.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
         }
     }
 }
---
+++ SnookerAcadWebApp/Helpers/ShortText.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SnookerAcadWebApp.Helpers
+{
+    public class ShortText
+    {
+        public static string CutText(string text, int maxLength = 200)
+        {
+            if (text.Length <= maxLength)
+            {
+                return text;
+            }
+
+            text = text.Substring(0, maxLength) + "...";
+            return text;
+        }
+    }
+}
